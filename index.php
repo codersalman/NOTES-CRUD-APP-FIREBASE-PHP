f7224a5564a09da58b4052681e50d90e46a79d93
codersalman <coder88salman@gmail.com>
2022-02-14 17:52:27 +0530

    Done NOtes app on firebase rtdb

--- index.php
+++ index.php
@@ -238,7 +238,6 @@ session_start();
             element.addEventListener("click", (e) => {
                 console.log("edit ");
                 tr = e.target.parentNode.parentNode;
-
                 title = tr.getElementsByTagName("td")[0].innerText;
                 description = tr.getElementsByTagName("td")[1].innerText;
                 console.log(title, description);
